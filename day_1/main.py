cecaf9aca5c5f569f449eac03fca925308925c69
Mahdi Dadkhah <Mahdi_Dadkhah@outlook.com>
2023-08-17 03:01:10 +0330

    exercies

--- day_1/main.py
+++ day_1/main.py
@@ -1,1 +1,36 @@
-print("Hello word")
+#exercies 1
+
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+#exercies 2
+
+# print(input("what is your name ?"))
+
+
+#exercies 3
+
+# print(len(input("what is your name ?")))
+
+
+#exercies 4
+
+# name = input("what is your name? ")
+# length = len(name)
+# print(length)
+
+
+#exercies 5
+
+# a = input("a: ")
+# b = input("b: ")
+
+
+# aa = a
+
+# a = b
+# b = aa
+
+# print("a: " + a)
+# print("b: " + b)
