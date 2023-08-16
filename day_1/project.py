bc6d6c76ae581b9b9da54a7701441624af52a333
Mahdi Dadkhah <Mahdi_Dadkhah@outlook.com>
2023-08-17 03:01:22 +0330

    day 1 project

---
+++ day_1/project.py
@@ -0,0 +1,16 @@
+#1 Create a greeting for your program
+#2 Ask the user for the city that they grow up in
+#3 Ask the user for the name of a pet
+#4 Combine the name of their city and pet and show them their band name
+#5 make sure the input cursor shown on a new line
+
+
+print("Hi , Welcome to our page.")
+
+city = input("Where did you grow up? ")
+pet = input("What is your pet name? ")
+
+band = city + " " + pet
+
+print("Your band name is: " + band)
+
